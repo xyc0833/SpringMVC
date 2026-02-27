3a2b94366c0ad2d78fae1c6cbb2d11e0e9cc7a7c
xyc0833 <2031362123@qq.com>
2026-02-27 11:23:08 +0800

    RestFul风格

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,50 +1,16 @@
 package com.example.controller;
 
-import com.example.entity.User;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.net.http.HttpResponse;
+import org.springframework.web.bind.annotation.*;
 
 
-@Controller//直接添加注解即可
+@RestController//直接添加注解即可
 public class HelloController {
 
-//    @RequestMapping(value = "/")
-//    public ModelAndView index(HttpServletResponse response, @CookieValue(value="JSESSIONID",required = false)String test){
-//        System.out.println("获取到cookie值为：" + test);
-//        response.addCookie(new Cookie("test","xyc01"));
-//        return new ModelAndView("index");  //返回ModelAndView对象，这里填入了视图的名称
-//    }
-
-    //重定向
-    @RequestMapping("/index")
-    public String index(){
-        return "redirect:home";
+    @RequestMapping("/index/{str}")
+    public String index(@PathVariable String str) {
+        System.out.println(str);
+        return "index";
     }
-
-    @RequestMapping("/home")
-    public String home(){
-        return "home";
-    }
-
-    //请求转发
-
-//    @RequestMapping("/index")
-//    public String index(){
-//        return "forward:home";
-//    }
-//
-//    @RequestMapping("/home")
-//    public String home(){
-//        return "home";
-//    }
 
 }
