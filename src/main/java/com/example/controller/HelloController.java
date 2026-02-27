adc52e5b056860076cb941023353b54d598e5399
xyc0833 <2031362123@qq.com>
2026-02-27 10:49:49 +0800

    学习@CookieValue相关参数

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,29 +1,27 @@
 package com.example.controller;
 
+import com.example.entity.User;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.net.http.HttpResponse;
+
 
 @Controller//直接添加注解即可
 public class HelloController {
-    //@RequestMapping(value = "/index", params = {"username", "password"})
-    //@RequestMapping(value = "/index" ,method = RequestMethod.POST)//直接填写访问路径
+
     @RequestMapping(value = "/")
-    public ModelAndView index(){
-        ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.getModel().put("name","helloxyc");
-        return modelAndView;  //返回ModelAndView对象，这里填入了视图的名称
-        //返回后会经过视图解析器进行处理
+    public ModelAndView index(HttpServletResponse response, @CookieValue(value="JSESSIONID",required = false)String test){
+        System.out.println("获取到cookie值为：" + test);
+        response.addCookie(new Cookie("test","xyc01"));
+        return new ModelAndView("index");  //返回ModelAndView对象，这里填入了视图的名称
     }
-    @RequestMapping(value = "/hello")
-    public ModelAndView hello(@RequestParam(value ="username",required = false,defaultValue = "伞兵一号") String username, HttpSession session){
-        System.out.println("接收请求参数" + username);
-        System.out.println(session.getAttribute("test"));
-        session.setAttribute("test","虎杖有人");
-        return new ModelAndView("index");//这里的ModelandView返回的是html页面
-    }
+
 }
---
+++ src/main/java/com/example/entity/User.java
@@ -0,0 +1,9 @@
+package com.example.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    String username;
+    String password;
+}
